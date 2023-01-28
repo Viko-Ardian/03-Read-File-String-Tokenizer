4957c20124df78a52db00e5b740eec9f80113ede
Viko Ardian <vikoardian97@gmail.com>
2023-01-29 04:50:27 +0700

    create project read file using string tokenizer

---
+++ src/App.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.StringTokenizer;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        FileReader fileReader = new FileReader("database.txt");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+        String data = bufferedReader.readLine();
+        int number = 0;
+
+        System.out.println("\n-----------------------------------------------------------------------");
+        System.out.println("| no\t| name\t\t\t| Profesion\t\t| Language");
+        System.out.println("-----------------------------------------------------------------------");
+        while (data != null) {
+            number++;
+            StringTokenizer stringTokenizer =new StringTokenizer(data, ",");
+            System.out.printf("|%2d",number);
+            System.out.printf("\t| %-20s", stringTokenizer.nextToken());
+            System.out.printf("\t| %s", stringTokenizer.nextToken());
+            System.out.printf("\t| %s", stringTokenizer.nextToken());
+            System.out.println();
+
+            data = bufferedReader.readLine();
+        }
+        System.out.println("-----------------------------------------------------------------------\n");
+    }
+}
